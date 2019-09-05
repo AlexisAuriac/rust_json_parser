ea168fae8289c4f8e89aa286be0faea26fdb350a
alexis <alexis.auriac@epitech.eu>
2019-09-05 12:51:55 +0200

    Lexing done

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,8 @@ pub enum GramSym {
     NtsValue,
     NtsArrayFirstVal,
     NtsArrayNextVal,
-    NtsProperty,
+    NtsFirstProperty,
+    NtsNextProperty,
 }
 
 pub fn get_rt() -> RuleTable<GramSym> {
@@ -42,26 +43,66 @@ pub fn get_rt() -> RuleTable<GramSym> {
         vec![(GramSym::NtsValue, false)],
         GramSym::TsEos,
         vec![
+            // Basic values
             Rule::new(GramSym::NtsValue, GramSym::TsNbr, vec![]),
             Rule::new(GramSym::NtsValue, GramSym::TsString, vec![]),
             Rule::new(GramSym::NtsValue, GramSym::TsBool, vec![]),
+            // Array syntax
             Rule::new(
                 GramSym::NtsValue,
-                GramSym::TsRBracket,
-                vec![(GramSym::NtsValue, true)],
+                GramSym::TsLBracket,
+                vec![
+                    (GramSym::NtsArrayFirstVal, true),
+                    (GramSym::TsRBracket, false),
+                ],
             ),
             Rule::new(
+                GramSym::NtsArrayFirstVal,
+                GramSym::TsNbr,
+                vec![(GramSym::NtsArrayNextVal, true)],
+            ),
+            Rule::new(
+                GramSym::NtsArrayFirstVal,
+                GramSym::TsString,
+                vec![(GramSym::NtsArrayNextVal, true)],
+            ),
+            Rule::new(
+                GramSym::NtsArrayFirstVal,
+                GramSym::TsBool,
+                vec![(GramSym::NtsArrayNextVal, true)],
+            ),
+            Rule::new(
+                GramSym::NtsArrayNextVal,
+                GramSym::TsComma,
+                vec![(GramSym::NtsValue, false), (GramSym::NtsArrayNextVal, true)],
+            ),
+            // Object syntax
+            Rule::new(
                 GramSym::NtsValue,
-                GramSym::TsRCurlyBraces,
+                GramSym::TsLCurlyBraces,
                 vec![
-                    (GramSym::NtsProperty, true),
+                    (GramSym::NtsFirstProperty, true),
                     (GramSym::TsRCurlyBraces, false),
                 ],
             ),
             Rule::new(
-                GramSym::NtsProperty,
+                GramSym::NtsFirstProperty,
                 GramSym::TsString,
-                vec![(GramSym::TsColon, false), (GramSym::NtsValue, false)],
+                vec![
+                    (GramSym::TsColon, false),
+                    (GramSym::NtsValue, false),
+                    (GramSym::NtsNextProperty, true),
+                ],
+            ),
+            Rule::new(
+                GramSym::NtsNextProperty,
+                GramSym::TsComma,
+                vec![
+                    (GramSym::TsString, false),
+                    (GramSym::TsColon, false),
+                    (GramSym::NtsValue, false),
+                    (GramSym::NtsNextProperty, true),
+                ],
             ),
         ],
     );
